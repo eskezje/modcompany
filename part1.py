59549d584188c705f6be552b7e8d51c3b29e22da
eskezje <114604325+eskezje@users.noreply.github.com>
2026-02-23 17:47:24 +0100

    transition matrix and p of z=1 given c

---
+++ part1.py
@@ -0,0 +1,35 @@
+# suggested values
+alpha_val = 0.9
+beta_val = 0.2
+small_gamma_val = 0.1
+lambda_0 = 1
+lambda_5 = 5
+
+n = 10
+big_T = 100
+
+
+
+# implemeting the transition matrix gamma 
+import numpy as np
+
+def create_transition_matrix(gamma, beta):
+    Gamma = np.array([
+            [(1-gamma)  ,0          ,gamma],
+            [0          ,(1-gamma)  ,gamma],
+            [(beta/2)   ,(beta/2)   ,(1-beta)]
+            ], dtype=float)
+    return Gamma
+
+print(create_transition_matrix(small_gamma_val, beta_val))
+
+# implemeting the conditional probability for Z_{t,i}
+def p_z1_given_c(alpha, c):
+    if c == 0:
+        return (1-alpha)
+    elif c == 1:
+        return alpha 
+    elif c == 2:
+        return (0.5)
+    else:
+        raise ValueError("c must be 0, 1 or 2")
