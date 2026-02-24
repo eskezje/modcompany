1dc0737ccf53c998b29fe86a3e3b49c4c987ed48
eskezje <114604325+eskezje@users.noreply.github.com>
2026-02-24 12:33:15 +0100

    almost done with part 1

--- part1.py
+++ part1.py
@@ -33,3 +33,17 @@ def p_z1_given_c(alpha, c):
         return (0.5)
     else:
         raise ValueError("c must be 0, 1 or 2")
+
+def simulate_c(T, gamma, beta, rng=None):
+    rng = np.random.default_rng() if rng is None else rng
+    Gamma = create_transition_matrix(gamma, beta)
+
+    C = np.empty(T, dtype=int)
+    C[0] =  2 
+
+    states = np.array([0, 1, 2], dtype=int)
+
+    for t in range(1, T):
+        C[t] = rng.choice(states, p=Gamma[C[t-1]])
+
+    return C
